aada829ed9e4e91a36c9529c537127684ceab98c
mdmahmudurrahman <mailtommrr@gmail.com>
2020-09-29 21:30:42 +0200

    Spring data REST: customize resource url and paging

--- src/main/java/com/springboot/app/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/app/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.springboot.app.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springboot.app.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
