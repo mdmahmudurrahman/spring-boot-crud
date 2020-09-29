8e54180a4989bfc27ddb050516c1e20b319ed3a7
mdmahmudurrahman <mailtommrr@gmail.com>
2020-09-29 14:34:45 +0200

    CRUD operation using JPA

--- src/main/java/com/springboot/app/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/app/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.springboot.app.cruddemo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +15,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	private EmployeeDAO employeeDAO;
 	
 	@Autowired
-	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
 		employeeDAO = theEmployeeDAO;
 	}
 	
@@ -41,5 +42,4 @@ public class EmployeeServiceImpl implements EmployeeService {
 	public void deleteById(int theId) {
 		employeeDAO.deleteById(theId);
 	}
-
 }
